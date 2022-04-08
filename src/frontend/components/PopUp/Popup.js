2a6462a1a9c64e40ca0f284b5aed8d192de8afdf
dhruvil <dhruvilsoni1703@gmail.com>
2022-04-08 15:00:08 +0530

    small changes

--- src/frontend/components/PopUp/Popup.js
+++ src/frontend/components/PopUp/Popup.js
@@ -1,26 +1,27 @@
-import React from 'react'
-import {Dialog, DialogActions} from '@material-ui/core';
+import React from 'react';
+import { Dialog, DialogActions } from '@material-ui/core';
 import { DialogContent, DialogTitle } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import { Button } from '@material-ui/core';
 import './Popup.css';
 
-
-
 export default function Popup(props) {
-    
-    const {title, childern, openPopup, setOpenPopup} = props;
-    
+    const { title, childern, openPopup, setOpenPopup } = props;
+
     const handleClose = () => {
         setOpenPopup(false);
-      };
+    };
 
     return (
         <Dialog open={openPopup} onClose={handleClose}>
             <DialogTitle>
-                <TextField id="outlined-search" label="Title"  sx={{ m: 1, width: '43ch' }}/>
+                <TextField
+                    id="outlined-search"
+                    label="Title"
+                    sx={{ m: 1, width: '43ch' }}
+                />
             </DialogTitle>
-            
+
             <DialogContent>
                 <TextField
                     id="outlined-multiline-static"
@@ -29,15 +30,12 @@ export default function Popup(props) {
                     rows={10}
                     sx={{ m: 1, width: '60ch' }}
                 />
-                
             </DialogContent>
             <DialogContent>
-            <Button variant="contained" size="medium" id="submit-button">
+                <Button variant="contained" size="medium" id="submit-button">
                     Submit
                 </Button>
             </DialogContent>
-            
         </Dialog>
-    )
-
+    );
 }
